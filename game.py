4d8a13f662d27934c9396047fe284768cf00f1c7
Ahmed Chelly <76244671+ChellyAhmed@users.noreply.github.com>
2023-11-07 15:59:10 +0100

    Update game.py

--- game.py
+++ game.py
@@ -24,3 +24,56 @@ def checkIfAnswerCorrect():
 #TODO
 def getScoreboard(): #Displays in the server console
     print("Getting scoreboard")
+
+
+def sendToClientByUserName(userName, message):
+    #TODO
+    print("Sending to " + userName + ": " + message)
+
+def sendToAllClients(message):
+    #TODO
+    print("Sending to all clients: " + message)
+
+def receiveFromUser():
+    #TODO: synchronize code with Amine
+    randomNumber= random.randint(0,5)
+    userName = "user" + str(randomNumber)
+
+def display(message): #Displays in the server console
+    #TODO
+    print(message)
+
+def initializeScoreBoard(): 
+    for user in clientsNames:
+        scoreBoard[user] = 0
+
+def playQuestion(): 
+    question = random.choice(questions)
+    sendTime = time.time()
+    sendToAllClients(question['question'])
+    sendToAllClients("A: " + question['A']) 
+    sendToAllClients("B: " + question['B'])
+    sendToAllClients("C: " + question['C'])
+    sendToAllClients("D: " + question['D'])
+
+    #Collect answers:
+    while(time.time() - sendTime < 15):
+        answer = receiveFromUser()
+        #TODO: Double check with Amine the implementation of receiveFromUser()
+        if (answer ):
+            if(answer['answer'] == question['correctKey']):
+                currentPoints = round(15 - (time.time() - sendTime))
+                scoreBoard[answer['userName']] += currentPoints
+                sendToClientByUserName(answer['userName'], "Correct!")
+            else:
+                sendToClientByUserName(answer['userName'], "Wrong! The correct answer is " + question['correctKey'] + ": " + question[question['correctKey']])
+    questions.remove(question)
+
+
+def StartRound(questionsPerRound): 
+    #TODO: Synchronize code between Ahmed and Amine
+    initializeScoreBoard()
+    for i in range(questionsPerRound):
+        playQuestion()
+    display("Round finished!")
+    getScoreBoard()
